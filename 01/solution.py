8c62e9112e063c60a42cec87f5be85823dc0a219
ceciliehundahl <ceciliehundahl@gmail.com>
2023-12-03 10:55:40 +0100

    Day 1 task 1 and 2 in Python

---
+++ 01/solution.py
@@ -0,0 +1,72 @@
+digits = {'one': 1,
+          'two': 2,
+          'three': 3,
+          'four': 4,
+          'five' : 5,
+          'six' : 6,
+          'seven' : 7,
+          'eight' : 8,
+          'nine' : 9
+          }
+
+def parse_digits(line: str) -> int:
+    """
+    Crude parsing from the strings to digits
+    """
+    parsed = ""
+    remaining = line
+    while len(remaining) > 0:
+        for d in digits:
+            if remaining.startswith(d):
+                parsed += str(digits[d])
+        parsed += remaining[0]
+        remaining = remaining [1:]
+    return parsed
+
+def get_calibation_value(line: str) -> int:
+    """
+    On each line, the calibration value can be found by combining the first
+    digit and the last digit (in that order) to form a single two-digit number.
+    """
+    first = None
+    last = None
+    for element in line:
+        try:
+            number = int(element)
+        except (TypeError, ValueError):
+            continue
+        if not first:
+            first = number
+            last = number
+        else:
+            last = number
+    if first and last:
+        return (first*10 + last)
+    else:
+        return 0
+
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+
+# TASK 1
+
+calibration_sum = 0
+
+for line in lines:
+    value = get_calibation_value(line)
+    calibration_sum += value
+#    print(f"{line}: {value}")
+
+print(calibration_sum)
+
+# TASK 2
+
+calibration_sum = 0
+
+for line in lines:
+    parsed = parse_digits(line)
+    value = get_calibation_value(parsed)
+    calibration_sum += value
+
+print(calibration_sum)
+
